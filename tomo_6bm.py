c51e051e876641bd4bcdac1cdb0c4fdef6451e18
Chen <zdms1987@hotmail.com>
2019-04-02 15:23:52 -0500

    add devices initialization

--- tomo_6bm.py
+++ tomo_6bm.py
@@ -24,27 +24,47 @@ from datetime  import datetime
 from docopt import docopt
 from tqdm   import tqdm
 from pymongo import MongoClient
+
+from devices import Shutter
+from devices import AreaDetector
 from utility import log_event
 from utility import load_config
 
-def free_scan(scan_config):
-    pass
+
+def init_devices(conf_devices):
+    """Initialize all devices described in the config"""
+    devices = {}
+    # -- init shutter
+    devices['shutter'] = Shutter.from_config(conf_devices['shutter'])
+    # -- init area detector
+    # NOTE:
+    #   no call is made to the hardwire until init() is called
+    devices['detector'] = AreaDetector.from_config(conf_devices['areaDetector'])
+    # -- init motors
+
+    return devices
+
+
+def init_scan(conf_scans):
+    """Preparation work for scan"""
+    scans = {}
+    
+    return scans
+
 
 # ----- Start -----
 if __name__ == "__main__":
+    # CLI parsing
     args = docopt(__doc__, version=0.1)
     in_dryrun = bool(args['--dry-run'])
     in_verbose = bool(not(args['--quiet']))
 
-    conf_devices = load_config(args['<DEVICES>'])
-    conf_scan = load_config(args['<SCAN>'])
-
-    print(conf_devices)
-    print(conf_scan)
-
     # Initialize all devices with given configurations
+    devices = init_devices(load_config(args['<DEVICES>']))
+    scans = init_scan(load_config(args['<SCAN>']))
 
     # Collect flat/white field images
+    
 
     # Collect projections (step motion)
 
