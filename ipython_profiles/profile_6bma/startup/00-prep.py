90129f8a6f58b6fc11c0c96de1978d1678748fc0
Chen <zdms1987@hotmail.com>
2019-04-17 15:04:00 -0500

    finished the prep script

--- ipython_profiles/profile_6bma/startup/00-prep.py
+++ ipython_profiles/profile_6bma/startup/00-prep.py
@@ -1,3 +1,4 @@
+# Setup script for using BlueSky at 6-BM-A
 print(f'Initializing IPython environment using {__file__}')
 
 # -----
@@ -24,7 +25,7 @@ import socket
 import getpass
 HOSTNAME = socket.gethostname() or 'localhost'
 USERNAME = getpass.getuser() or '6-BM-A user'
-RE.md['beamline_id'] = 'APS 6-BM-B'
+RE.md['beamline_id'] = 'APS 6-BM-A'
 RE.md['proposal_id'] = 'internal test'
 RE.md['pid'] = os.getpid()
 RE.md['login_id'] = USERNAME + '@' + HOSTNAME
@@ -33,3 +34,35 @@ RE.md['OPHYD_VERSION'] = ophyd.__version__
 RE.md['apstools_VERSION'] = apstools.__version__
 RE.md['SESSION_STARTED'] = datetime.isoformat(datetime.now(), " ")
 
+print("*** checking beam status")
+import apstools.devices as APS_devices
+aps = APS_devices.ApsMachineParametersDevice(name="APS")
+
+from ophyd import EpicsSignalRO
+instrument_in_use = lambda : EpicsSignalRO("6bm:instrument_in_use", 
+                                           name="instrument_in_use",
+                                ).get()
+try:
+    RE.md['INSTRUMENT_IN_USE'] = instrument_in_use()
+except TimeoutError as e:
+    print(f"---receving error: {e}---")
+    print("---switching to offline test mode---")
+    offline_testmode = True
+    RE.md['INSTRUMENT_IN_USE'] = False
+finally:
+    from pprint import pprint
+    print("Current registered metadata:")
+    pprint(RE.md)
+    print("Update entries in RE.md if necessary")
+
+import apstools.synApps_ophyd
+calcs = apstools.synApps_ophyd.userCalcsDevice("6bma1:", name="calcs", )
+hutch_light_on = lambda : bool(calcs.calc1.val.get())
+
+# conducting experiment mode
+in_production = aps.inUserOperations \
+            and (instrument_in_use.get() in (1, "6-BM-A")) \
+            and (not hutch_light_on)
+
+# testing mode, supercede in_production
+in_dryrun = True
