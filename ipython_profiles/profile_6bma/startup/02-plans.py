252e60ee5717d92c20fe500ea074d1c58e3ca7a2
Chen <zdms1987@hotmail.com>
2019-04-18 14:37:19 -0500

    place holder for plans

---
+++ ipython_profiles/profile_6bma/startup/02-plans.py
@@ -0,0 +1,21 @@
+# prefined plans for tomo scan at 6bma
+_sep = u"🙊"*30
+print(f'{_sep}\nCaching pre-defined scan plans with {__file__}')
+
+import bluesky.plans         as bp
+import bluesky.preprocessors as bpp
+import bluesky.plan_stubs    as bps
+
+
+# ----
+# Dark/White field
+
+
+# ----
+# Projections (step)
+
+
+# ----
+# Projections (fly)
+
+print(f"Done with {__file__}\n{_sep}\n")
