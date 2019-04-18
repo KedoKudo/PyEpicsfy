229b6eb64b2062dbfc42b1b26a2d7a8fe8b63749
Chen <zdms1987@hotmail.com>
2019-04-18 16:04:58 -0500

    add step scan plan

--- ipython_profiles/profile_6bma/startup/02-plans.py
+++ ipython_profiles/profile_6bma/startup/02-plans.py
@@ -2,6 +2,7 @@
 _sep = u"🙊"*30
 print(f'{_sep}\nCaching pre-defined scan plans with {__file__}')
 
+import numpy                 as np
 import bluesky.plans         as bp
 import bluesky.preprocessors as bpp
 import bluesky.plan_stubs    as bps
@@ -9,51 +10,87 @@ import bluesky.plan_stubs    as bps
 
 # ----
 # Dark/White flat field
+# NOTE:
+#    det is a global var that refers to the detector
 def collect_background(n_images, 
                        n_frames,
-                       detector,
                        output='tiff',
     ):
     """collect n_images backgrounds with n_frames per take"""
-    yield from bps.mv(detector.cam.acquire, 0)
+    yield from bps.mv(det.cam.acquire, 0)
 
     if output.lower() in ['tif', 'tiff']:
         for k,v in {
             "num_capture": n_images,
             "capture":     1,
-        }.items(): detector.tiff1.stage_sigs[k] = v
+        }.items(): det.tiff1.stage_sigs[k] = v
     elif output.lower() in ['hdf', 'hdf1', 'hdf5']:
         for k,v in {
             "num_capture": n_images,
             "capture":     1,
-        }.items(): detector.hdf1.stage_sigs[k] = v
+        }.items(): det.hdf1.stage_sigs[k] = v
     else:
         raise ValueError(f"Unknown output format {output}")
     
     for k,v in {
         "reset_filter":     1,
         "num_filter":       n_frames,
-    }.items(): detector.proc1.stage_sigs[k] = v
+    }.items(): det.proc1.stage_sigs[k] = v
         
     for k,v in {
         "trigger_mode": "Internal",
         "image_mode":   "Multiple",
         "num_images":   n_frames,
-    }.items(): detector.cam.stage_sigs[k] = v
+    }.items(): det.cam.stage_sigs[k] = v
 
-    @bpp.stage_decorator([detector])
+    @bpp.stage_decorator([det])
     @bpp.run_decorator()
     def scan():
-        yield from bps.trigger_and_read([detector]) 
+        yield from bps.trigger_and_read([det]) 
     
     return (yield from scan())
 
 
 # ----
 # Projections (step)
-def step_scan():
+def step_scan(n_images, 
+              n_frames,
+              angs,           # list of angles where images are taken
+              output='tiff',
+    ):
     """collect proejctions by stepping motors"""
-    pass
+    if output.lower() in ['tif', 'tiff']:
+        for k,v in {
+            "num_capture": n_images,
+            "capture":     1,
+        }.items(): det.tiff1.stage_sigs[k] = v
+    elif output.lower() in ['hdf', 'hdf1', 'hdf5']:
+        for k,v in {
+            "num_capture": n_images,
+            "capture":     1,
+        }.items(): det.hdf1.stage_sigs[k] = v
+    else:
+        raise ValueError(f"Unknown output format {output}")
+
+    for k, v in {
+        "enable":           1,         # toggle on proc1
+        "reset_filter":     1,         # reset number_filtered
+        "num_filter":       n_frames,
+    }.items(): det.proc1.stage_sigs[k] = v
+    
+    for k, v in {
+        "num_images":   n_frames,      
+    }.items(): det.cam.stage_sigs[k] = v
+
+    @bpp.stage_decorator([det])
+    @bpp.run_decorator()
+    def scan_closure():
+        for ang in angs:
+            yield from bps.checkpoint()
+            yield from bps.mv(preci, ang)
+            yield from bps.trigger_and_read([det])
+    
+    return (yield from scan_closure())
 
 
 # ----
@@ -65,7 +102,20 @@ def fly_scan():
 
 # ----
 # Example bundled tomo characterization scan
-def tomo_step():
+config_tomo_step = {
+    "n_white"        :  10,
+    "n_dark"         :  10,
+    "samOutDist"     : -5.00,           # mm
+    "omega_step"     :  0.25,           # degrees
+    "acquire_time"   :  0.05,           # sec
+    "acquire_period" :  0.05+0.01,      # sec
+    "time_wait"      : (0.05+0.01)*2,   # sec
+    "omega_start"    :  -180,           # degrees
+    "omega_end"      :  180,            # degrees
+    "n_frames"       :  5,              # proc.n_filters, cam.n_images
+    "output"         : "tiff",          # output format ['tiff', 'hdf5']
+}
+def tomo_step(config_dict):
     """
     The master plan pass to RE for
     
@@ -73,6 +123,31 @@ def tomo_step():
     2. projection collection
     3. post-white-field background collection
     4. post-dark-field background collection
+
+    NOTE:
+    see config_tomo_step for key inputs
+    """
+    pass
+
+
+
+# ----
+# Fly scan bundle example
+config_tomo_step = {
+    "n_white"        :  10,
+    "n_dark"         :  10,
+}
+def tomo_fly(config_dict):
+    """
+    The master plan pass to RE for
+    
+    1. pre-white-field background collection
+    2. projection collection
+    3. post-white-field background collection
+    4. post-dark-field background collection
+
+    NOTE:
+    see config_tomo_fly for key inputs
     """
     pass
 
