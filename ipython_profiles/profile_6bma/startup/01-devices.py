ed5d4b6420986b61c9e43cf39c797e39b65ca9d2
Chen <zdms1987@hotmail.com>
2019-04-23 16:48:57 -0500

    quick hack

--- ipython_profiles/profile_6bma/startup/01-devices.py
+++ ipython_profiles/profile_6bma/startup/01-devices.py
@@ -130,7 +130,7 @@ class PointGreyDetector6BM(SingleTrigger, AreaDetector):
             MyHDF5Plugin, 
             suffix="HDF1:",
             # root='/',                          # for databroker
-            # write_path_template=FILE_PATH,     # for EPICS AD
+            write_path_template=r"Y:\2019\startup_apr19\tomo\test",     # for EPICS AD
         )
 
 # Area Detector (AD) config block
